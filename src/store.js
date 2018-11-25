439e0fb9f33a34c42a3e9f20a4b6eb35f678133f
Trevor Jacklitch <trevorjacklitch123@gmail.com>
2018-11-24 19:53:34 -0600

    Added redux chrome dev tools

--- src/store.js
+++ src/store.js
@@ -1,15 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 import rootReducer from './reducers/index.js';
 
 const initialState = {};
 
-const middleware = [thunk];
-
 const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(...middleware)
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
